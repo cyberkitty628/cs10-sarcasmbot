b0d3c6e2a9105890ec5af3831326da364219d52c
Kristina <cyberkitty628@gmail.com>
2020-09-20 14:09:47 -0700

    Update sarcasmbot.py
    Printed welcome message and added loop, input and bot response

--- sarcasmbot.py
+++ sarcasmbot.py
@@ -4,7 +4,7 @@ def get_bot_response(user_response):
 
     bot_response_good = ["Good for you.", "That's just grand.", "Must be nice."]
     bot_response_bad = ["What else is new?", "Well, aren't you just a little ray of sunshine?", "Here we go again..."]
-    bot_resonse_fine = ["If you say so, pal.", "Honestly, though?", "Sure, buddy."]
+    bot_response_fine = ["If you say so, pal.", "Honestly, though?", "Sure, buddy."]
 
     if user_response == "good":
         return choice(bot_response_good)
@@ -14,3 +14,15 @@ def get_bot_response(user_response):
         return choice(bot_response_fine)
     else:
         return "Come again?"
+    
+    print("Welcome to Sarcasm Bot! ...Don't take it personal.")
+    print("Enter \"done\" to quit.")
+
+while True:
+    user_response = input("How are you feeling today? ")
+
+    if user_response == "done":
+        break
+
+    bot_response = get_bot_response(user_response)
+    print(bot_response)
