c2a3a90256b28d04d3a159b5d9ac8b8fb67a14ec
Kristina <cyberkitty628@gmail.com>
2020-09-20 15:44:12 -0700

    Update sarcasmbot.py
    Fixed code with Joi

--- sarcasmbot.py
+++ sarcasmbot.py
@@ -2,27 +2,45 @@ from random import choice
 
 def get_bot_response(user_response):
 
-    bot_response_good = ["Good for you.", "That's just grand.", "Must be nice."]
-    bot_response_bad = ["What else is new?", "Well, aren't you just a little ray of sunshine?", "Here we go again..."]
-    bot_response_fine = ["If you say so, pal.", "Honestly, though?", "Sure, buddy."]
+  bot_response_good = ["Good for you.", "That's just grand.", "Must be nice."]
+  bot_response_bad = ["What else is new?", "Well, aren't you just a little ray of sunshine?", "Here we go again..."]
+  bot_response_fine = ["If you say so.", "Honestly, though?", "Sure, buddy."]
+  bot_response_done = ["Good riddance.", "Hate to see you go, love to watch you leave.", "Don't let the door hit you on the way out."]
+  bot_response_else = ["Come again?", "Sorry, I don't speak nerd.", "What did you just say to me?", "Does not compute."]
 
-    if user_response == "good":
-        return choice(bot_response_good)
-    elif user_response == "bad":
-        return choice(bot_response_bad)
-    elif user_response == "fine":
-        return choice(bot_response_fine)
-    else:
-        return "Come again?"
-    
-    print("Welcome to Sarcasm Bot! ...Don't take it personal.")
-    print("Enter \"done\" to quit.")
+  if user_response == "good":
+    return choice(bot_response_good)
+  elif user_response == "bad":
+    return choice(bot_response_bad)
+  elif user_response == "fine":
+    return choice(bot_response_fine)
+  elif user_response == "done":
+    return choice(bot_response_done)
+  else:
+    return choice(bot_response_else)
+
+def get_bot_response2(user_response2):
+
+  bot_response2 = ["Alright, we get it.", "Turn it down a notch.", "You bore me.", "I'm done playing this game."]
+  
+  return choice(bot_response2)
+
+# Sarcasm Bot:
+
+# Welcomes to user to the Sarcasm chat bot.
+print("Welcome to Sarcasm Bot! ...Don't take it personal.")
+print("Enter \"done\" to quit.")
 
 while True:
-    user_response = input("How are you feeling today? ")
 
-    if user_response == "done":
-        break
+  user_response = input("How are you feeling today?: ")
+  bot_response = get_bot_response(user_response)
+  print(bot_response)
 
-    bot_response = get_bot_response(user_response)
-    print(bot_response)
+  if user_response == "done":
+    break
+  
+  user_response2 = input("Anything else? ")
+  bot_response2 = get_bot_response2(user_response2)
+  print(bot_response2)
+  break
